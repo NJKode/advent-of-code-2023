8ebdd71b8f65f3274bffbb78f9e598b3d3207bad
Nathan Kennedy <nathan@cloudcannon.com>
2023-12-12 17:51:39 +1300

    Boiler-plate for remaining days

---
+++ day12/python/p2.py
@@ -0,0 +1,3 @@
+text_file = open("input-p2.txt", "r")
+input = text_file.read()
+text_file.close()
---
+++ day15/python/p1.py
@@ -0,0 +1,3 @@
+text_file = open("input-p1.txt", "r")
+input = text_file.read()
+text_file.close()
